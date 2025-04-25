358b6fed056862cc71eff80b93f263d5a1e2135f
Craig Clayton <craig__clayton@outlook.com>
2025-04-25 14:50:07 +0100

    get all tasks working on server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+dotenv.config();
+
+const dbConnectionString = process.env.DATABASE_URL;
+
+const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>
+  res.send(
+    '<h1 style="color: red; text-align: center;">This is the root route</h1>'
+  )
+);
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const { rows } = await db.query(`SELECT * FROM tasks`);
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: 'Server is down :(' });
+  }
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));

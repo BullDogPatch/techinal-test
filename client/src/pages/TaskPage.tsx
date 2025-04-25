39a68a3e596da86148870d3f6e2042971e3bfffb
Craig Clayton <craig__clayton@outlook.com>
2025-04-25 16:32:26 +0100

    add route for task on its own page

--- client/src/pages/TaskPage.tsx
+++ client/src/pages/TaskPage.tsx
@@ -1,5 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { SingleTask } from '../App';
+
+const fetchTask = async (id: string | undefined): Promise<SingleTask> => {
+  const response = await fetch(`http://localhost:8080/tasks/${id}`);
+  const data = await response.json();
+  return data;
+};
+
 const TaskPage = () => {
-  return <div>TaskPage</div>;
+  const { id } = useParams();
+  const { data: task } = useQuery<SingleTask>({
+    queryKey: ['task', id],
+    queryFn: () => fetchTask(id),
+  });
+
+  return (
+    <div>
+      <p className='text-2xl'>{task?.title}</p>
+      <p>{task?.description}</p>
+      <p> Status: {task?.status}</p>
+      <p>Due Date: {task?.due_date}</p>
+    </div>
+  );
 };
 
 export default TaskPage;

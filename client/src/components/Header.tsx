e0daadfb778676c23ddc1360952529f3d32ea3c0
Craig Clayton <craig__clayton@outlook.com>
2025-04-26 17:06:52 +0100

    change text in header to be larger on medium screens and up

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,9 @@ import { FaPlus } from 'react-icons/fa';
 const Header = () => {
   return (
     <header className='flex justify-between items-center px-3 py-4'>
-      <h1 className='text-3xl font-bold'>Task Tracker</h1>
+      <h1 className='text-xl font-bold md:text-2xl lg:text-3xl'>
+        Task Tracker
+      </h1>
       <Link
         to='/create'
         className='flex items-center gap-2 bg-white text-blue-600 font-semibold px-3 py-1 rounded-md hover:bg-gray-100 transition hover:shadow-md hover:scale-105'

82c3cf17d9808b9a876cbb01ab7e6dca3a882483
Craig Clayton <craig__clayton@outlook.com>
2025-04-26 17:04:09 +0100

    update task so its a link to single task route

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { SingleTask } from '../App';
 import { deleteTask } from '../utils/api';
 import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
 
 const Task = ({ task }: { task: SingleTask }) => {
   const queryClient = useQueryClient();
@@ -24,13 +25,18 @@ const Task = ({ task }: { task: SingleTask }) => {
 
   return (
     <div className='m-6'>
-      <p className='text-2xl'>{task.title}</p>
-      <p>{task.description}</p>
-      <p>Status: {task.status}</p>
-      <p>Due Date: {task.due_date}</p>
+      <Link to={`/task/${task.id}`}>
+        <div className='hover:shadow-md hover:bg-gray-800 transition-all p-4 rounded-md'>
+          <p className='text-2xl font-bold'>{task.title}</p>
+          <p className='text-gray-600'>{task.description}</p>
+          <p className='text-sm text-gray-500'>Status: {task.status}</p>
+          <p className='text-sm text-gray-500'>Due Date: {task.due_date}</p>
+        </div>
+      </Link>
+
       <button
         onClick={() => mutate(task.id)}
-        className='cursor-pointer bg-red-600 px-2 py-1 rounded-md'
+        className='cursor-pointer mt-2 bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-700'
       >
         Delete
       </button>

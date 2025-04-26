f8aa0240f82dfcdd0d42fe41659df45ed115897f
Craig Clayton <craig__clayton@outlook.com>
2025-04-26 16:54:12 +0100

    add hover affect to link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,11 +3,11 @@ import { FaPlus } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header className='flex justify-between items-center '>
+    <header className='flex justify-between items-center px-3 py-4'>
       <h1 className='text-3xl font-bold'>Task Tracker</h1>
       <Link
         to='/create'
-        className='flex items-center gap-2 bg-white text-blue-600 font-semibold px-3 py-1 rounded-md hover:bg-gray-100 transition'
+        className='flex items-center gap-2 bg-white text-blue-600 font-semibold px-3 py-1 rounded-md hover:bg-gray-100 transition hover:shadow-md hover:scale-105'
       >
         <FaPlus />
         <span>Create</span>

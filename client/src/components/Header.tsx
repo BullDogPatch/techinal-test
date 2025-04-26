ee73418b02066377502e074f05fd6d930b7c7828
Craig Clayton <craig__clayton@outlook.com>
2025-04-26 16:38:56 +0100

    Link in header to create task form

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
+
 const Header = () => {
   return (
-    <header>
+    <header className='flex justify-between items-center '>
       <h1 className='text-3xl font-bold'>Task Tracker</h1>
+      <Link
+        to='/create'
+        className='flex items-center gap-2 bg-white text-blue-600 font-semibold px-3 py-1 rounded-md hover:bg-gray-100 transition'
+      >
+        <FaPlus />
+        <span>Create</span>
+      </Link>
     </header>
   );
 };

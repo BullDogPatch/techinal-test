6656525e9566d823f5d5c6dcce5fb73e95abda5e
Craig Clayton <craig__clayton@outlook.com>
2025-04-27 14:08:12 +0100

    render out option drop down for status'

--- client/src/pages/TaskPage.tsx
+++ client/src/pages/TaskPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { SingleTask } from '../App';
 import { fetchTask } from '../utils/api';
 
+const TASKS_STATUS = ['todo', 'in-progress', 'done'];
+
 const TaskPage = () => {
   const { id } = useParams();
   const { data: task, isPending } = useQuery<SingleTask>({
@@ -16,7 +18,13 @@ const TaskPage = () => {
     <div>
       <p className='text-2xl'>{task?.title}</p>
       <p>{task?.description}</p>
-      <p> Status: {task?.status}</p>
+      <span>{task?.status}</span>
+      <select name='' id=''>
+        <option value=''>--choose status--</option>
+        {TASKS_STATUS.map((status) => (
+          <option>{status}</option>
+        ))}
+      </select>
       <p>Due Date: {task?.due_date}</p>
     </div>
   );

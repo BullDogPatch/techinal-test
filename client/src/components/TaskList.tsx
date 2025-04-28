7f6e59b2c82e7f24c9170350f0a18db583b9e738
Craig Clayton <craig__clayton@outlook.com>
2025-04-28 15:04:24 +0100

    change react query to refetch every 5 seconds

--- client/src/components/TaskList.tsx
+++ client/src/components/TaskList.tsx
@@ -9,7 +9,7 @@ const TaskList = () => {
     queryKey: ['tasks'],
     queryFn: fetchTasks,
     staleTime: 1000 * 60 * 5,
-    refetchInterval: 1000,
+    refetchInterval: 5000,
   });
 
   if (isPending) return <p>Loading...</p>;

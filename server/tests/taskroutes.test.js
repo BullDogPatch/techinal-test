e6d8670d22441bcf4e67da3e51e70d9727d84d1c
Craig Clayton <craig__clayton@outlook.com>
2025-04-28 15:02:01 +0100

    create test for /tasks/:id

--- server/tests/taskroutes.test.js
+++ server/tests/taskroutes.test.js
@@ -15,4 +15,35 @@ describe('Task API', () => {
     });
     expect(res.statusCode).toBe(400);
   });
+  it('should fail when creating task without due_date', async () => {
+    const res = await request(app).post('/create-task').send({
+      title: 'Some title',
+      description: 'some description',
+      status: 'todo',
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+describe('Task API', () => {
+  it('should return 200 OK and task data on GET /tasks/:id', async () => {
+    const res = await request(app).get('/tasks/1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body.id).toEqual(1);
+    expect(res.body).toHaveProperty('title');
+    expect(res.body).toHaveProperty('description');
+  });
+
+  it('should return 404 if task not found on GET /tasks/:id', async () => {
+    const res = await request(app).get('/tasks/999');
+    expect(res.statusCode).toEqual(404);
+    expect(res.body.message).toBe('Task not found');
+  });
+
+  // it('should return 500 for internal server errors on GET /tasks/:id', async () => {
+  //   const res = await request(app).get('/tasks/1');
+  //   expect(res.statusCode).toEqual(500);
+  //   expect(res.body.message).toBe('Internal server error');
+  // });
 });
